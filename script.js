07f40b497db9c61ae43d42978389f5684bc15abb
Emmanuel Lyimo <esal29t@gmail.com>
2021-11-18 15:01:10 -0800

    renamed index.html and modified script.js

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+
 //Password characters
 
 var numbers=["0","1","2","3","4","5","6","7","8","8","9"];
 
 var lowerCaseLetters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-var upperCase=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var upperCaseLetters=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
-var specialCharacters=[]
+var specialCharacters=["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", "]"]
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var passwordLength = prompt("How many charactors do you want your password to contain?");
+  console.log("passwordLength",passwordLength); 
+  // var password = generatePassword();
+
+  
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -22,3 +34,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+    
+}
